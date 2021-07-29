53a80b6557a8b6ef68f722dcfbeb364077fe2998
Jeonghyun Kim <kimdictor@gmail.com>
2021-07-29 16:23:01 +0900

    오타 수정

--- lecture4-dac-sinewave/lecture4-dac-sinewave.ino
+++ lecture4-dac-sinewave/lecture4-dac-sinewave.ino
@@ -5,7 +5,7 @@ void setup() {
 }
 
 void loop() {
-    for (float x; x < 2 * M_PI; x += 0.05f) {
+    for (float x = 0.0f; x < 2 * M_PI; x += 0.05f) {
         analogWrite(DAC0, (int)((1 + sinf(x)) * 2048));
     }
 }
